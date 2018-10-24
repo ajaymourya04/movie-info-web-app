2857a28ed59a5dae384fbe7026f3bdab32362308
ajaymourya1234 <ajaymourya8553@gmail.com>
2018-10-24 09:59:12 +0530

    replaced http with https in script.js for solving issue related to gh-pages

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   // The base url for all API calls
-  var apiBaseURL = 'http://api.themoviedb.org/3/';
+  var apiBaseURL = 'https://api.themoviedb.org/3/';
 
   // URL in Authentication. Base URL of image
   var imageBaseUrl = 'https://image.tmdb.org/t/p/';
